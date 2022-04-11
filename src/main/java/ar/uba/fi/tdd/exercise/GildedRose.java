2ea5b5bf3baabd77b2163241b653bac92ab93f9f
Cbravor1991 <cbravor@fi.uba.ar>
2022-04-10 22:12:29 -0300

    se ordena codigo existente creando funciones nuevas

--- src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
+++ src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
@@ -8,12 +8,47 @@ class GildedRose {
         items = _items;
     }
 
-    // update the quality of the emements
+
+    public void disminuirCalidadAgedBrieCaso(Item item){
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                // si es menor que 6
+                if (item.sellIn < 6) {
+                    // si la cantidad es es menor que 50
+                    if (item.quality < 50) {
+                        // sumo 1 a la calidad
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public void disminuirCalidaCasoDefault(Item item) {
+        if (!item.Name.equals("Aged Brie")
+                && !item.Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0)
+                if (!item.Name.equals("Sulfuras, Hand of Ragnaros"))
+                    item.quality = item.quality - 1;
+        } else {
+            disminuirCalidadAgedBrieCaso(item);
+        }
+
+    }
+        // update the quality of the emements
     public void updateQuality() {
         // for each item
         for (int i = 0; i < items.length; i++) {
+            disminuirCalidaCasoDefault(items[i]);
 
-            if (!items[i].Name.equals("Aged Brie")
+            /*if (!items[i].Name.equals("Aged Brie")
                     && !items[i].Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0)
                     if (!items[i].Name.equals("Sulfuras, Hand of Ragnaros"))
@@ -37,7 +72,7 @@ class GildedRose {
                             }
                         }
                     }
-            }}
+            }}*/
 
             if (!items[i].Name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
