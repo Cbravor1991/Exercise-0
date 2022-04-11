1b2924a0e51d0ee86c49833579219afd082f0bc5
Cbravor1991 <cbravor@fi.uba.ar>
2022-04-11 16:49:12 -0300

    codificacion caso aged brie

--- src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
+++ src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
@@ -98,7 +98,7 @@ class GildedRose {
 
             //caso backstage passes
             if(items[i].Name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                if(items[i].quality >0){
+                if(items[i].quality <50){
                     if(items[i].sellIn <11 && items[i].sellIn >3  ){
                         items[i].quality = items[i].quality + 2;
 
@@ -113,6 +113,13 @@ class GildedRose {
 
                 }
             } // fin caso backstage passes
+
+            //caso aged brief
+            if(items[i].Name.equals("Aged Brie")){
+                if(items[i].quality <50){
+                    items[i].quality = items[i].quality + 1;
+                }
+            } // fin caso aged brief
 
         } //termina eñ for
     }
