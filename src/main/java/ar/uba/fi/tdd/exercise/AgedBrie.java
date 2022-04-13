2fa44d57e13eb75c6077d1aba94a19ae0b3eee0d
Cbravor1991 <cbravor@fi.uba.ar>
2022-04-13 17:29:59 -0300

    nueva clase AgedBrie

---
+++ src/main/java/ar/uba/fi/tdd/exercise/AgedBrie.java
@@ -0,0 +1,14 @@
+package ar.uba.fi.tdd.exercise;
+
+public class AgedBrie {
+
+    Quality calidad = new Quality();
+
+    public AgedBrie (){}
+
+    public void modificarCalidad(Item item){
+        if(item.quality <50){
+            calidad.aumentarCalidad(item);
+        }
+    }
+}
