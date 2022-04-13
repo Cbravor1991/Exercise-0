e561c53ccea9dad1b24524bbdf8363793d70faba
Cbravor1991 <cbravor@fi.uba.ar>
2022-04-13 18:25:02 -0300

    delego comportamiento de GildedRoses en las clases creadas

--- src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
+++ src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
@@ -4,6 +4,10 @@ package ar.uba.fi.tdd.exercise;
 class GildedRose {
   Item[] items;
   Quality calidad = new Quality();
+  Backstage backstage = new Backstage();
+  AgedBrie agedBrie = new AgedBrie();
+  Sulfuras sulfuras = new Sulfuras();
+  Conjured conjured = new Conjured();
 
     public GildedRose(Item[] _items) {
         items = _items;
@@ -18,42 +22,24 @@ class GildedRose {
 
             //caso backstage passes
             if(items[i].Name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                if(items[i].quality <50){
-                    if(items[i].sellIn <11 && items[i].sellIn >3  ){
-                        calidad.duplicarCalidad(items[i]);
-
-                    }
-
-                    if(items[i].sellIn <6 && items[i].sellIn >0  ){
-                        calidad.triplicarCalidad(items[i]);
-                    }
-
-                    if(items[i].sellIn <=0){
-                        calidad.calidadNula(items[i]);
-
-                    }
-
-
-                }
+                backstage.modificarCalidad(items[i]);
             } // fin caso backstage passes
 
             //caso aged brief
             if(items[i].Name.equals("Aged Brie")){
-                if(items[i].quality <50){
-                    calidad.aumentarCalidad(items[i]);
-                }
+                agedBrie.modificarCalidad(items[i]);
                 // fin caso aged brief
             }
             //caso Sulfuras
 
 
             if(items[i].Name.equals("Sulfuras, Hand of Ragnaros")){
-                items[i].sellIn = items[i].sellIn -1;
+                sulfuras.modificarSellin(items[i]);
 
                 }
             //caso conjured
             if(items[i].Name.equals("conjured")){
-                calidad.disminuyeCalidadPorDos(items[i]);
+                conjured.modificarCalidad(items[i]);
             }
 
 
